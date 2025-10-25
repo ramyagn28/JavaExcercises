f67a011152e1789a0a5c68eb7bc6108a775922f9
Ramya G <ramya.28gn@gmail.com>
2025-10-25 12:43:36 -0400

    Implement sealed classes with Circle and Square

--- Java17Features.java
+++ Java17Features.java
@@ -1,1 +1,23 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        Shape s = new Circle();
+        s.print();
+    }
+}
 
+sealed class Shape permits Circle, Square {
+    public void print() {
+        System.out.println("Inside Shape class print");
+    }
+} 
+non-sealed class Circle extends Shape {
+    public void print() {
+        System.out.println("Inside Circle class print");
+    }
+}
+non-sealed class Square extends Shape {
+    public void print() {
+        System.out.println("Inside Square class print");
+    }
+}
